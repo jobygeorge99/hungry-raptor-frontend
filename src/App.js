0437cb187be23c7d066a85b6b8c3ce3d69cb4e59
jobygeorge99 <jobygeorge995@gmail.com>
2024-04-04 14:54:42 +0530

    initial-code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddDish from './components/AddDish';
 import RemoveDish from './components/RemoveDish';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/addDish' element={<AddDish/>} />
           <Route path='/removeDish' element={<RemoveDish/>} />
+          <Route path='/login' element={<Login/>} />
         </Routes>
       </BrowserRouter>
     </div>
