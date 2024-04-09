f2d48c8b95b3abd30e19c6b57cb25d94592e2eea
jobygeorge99 <jobygeorge995@gmail.com>
2024-04-09 13:42:43 +0530

    changes

--- src/components/userScreens/ViewMenu.jsx
+++ src/components/userScreens/ViewMenu.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React,{ useEffect,useState } from 'react'
 import UserNavBar from './UserNavBar'
+import axios from 'axios'
+import QuantityPicker from '../quantitySelector/QuantityPicker'
 
 const ViewMenu = () => {
+
+  const [data,setData] = new useState([])
+
+  const getData = ()=>{
+      axios.get("http://localhost:3001/api/user/viewMenu").then((response)=>{
+          setData(response.data)
+          console.log(response.data)
+      })
+  }
+
+  const addToCart = ()=>{
+
+  }
+
+  useEffect(()=>{getData()},[])
+
   return (
     <div>
         <UserNavBar/>
@@ -10,7 +28,24 @@ const ViewMenu = () => {
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                     <div className="row g-3">
-                        
+                      {
+                        data.map((value,index)=>{
+                          return <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3 d-flex">
+                                    <div class="card">
+                                      <img src={value.image} style={{ width: '200px', height: '200px' }} class="card-img-top" alt="..."/>
+                                      <div class="card-body">
+                                        <h5 className="card-title">{value.name}</h5>
+                                        <p className="card-text">Price: {value.price}</p>
+                                        <div className="div mb-2"> <QuantityPicker min={0} max={10} /> </div>
+                                        <div>
+                                          {console.log(this.State.value)}
+                                        </div>
+                                        <div className="btn btn-primary d-flex justify-content-center" onClick={ ()=>addToCart(value._id) } > Add to Cart </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                        })
+                      }
                     </div>
 
                 </div>
